3170b01dd3797e9f0230a4833141aad3fde12740
kennedyrs <kennedywork202@gmail.com>
2021-03-27 13:05:13 -0300

    Dia #04

--- client/src/eventManager.js
+++ client/src/eventManager.js
@@ -24,27 +24,35 @@ export default class EventManager {
     this.#updateUsersComponent();
   }
 
+  disconnectUser(user) {
+    const { userName, id } = user;
+    this.#allUsers.delete(id);
+
+    this.#updateActivityLogComponent(`${userName} left!`);
+    this.#updateUsersComponent();
+  }
+
+  message(message) {
+    this.componentEmitter.emit(constants.events.app.MESSAGE_RECEIVED, message);
+  }
+
   newUserConnected(message) {
     const user = message;
     this.#allUsers.set(user.id, user.userName);
 
     this.#updateUsersComponent();
-    this.#updateActivityComponent(`${user.userName} joined!`);
+    this.#updateActivityLogComponent(`${user.userName} joined!`);
   }
 
-  #emitComponentUpdate(event, message) {
-    this.componentEmitter.emit(event, message);
-  }
-
-  #updateActivityComponent(message) {
-    this.#emitComponentUpdate(
+  #updateActivityLogComponent(message) {
+    this.componentEmitter.emit(
       constants.events.app.ACTIVITYLOG_UPDATED,
       message
     );
   }
 
   #updateUsersComponent(allUsers) {
-    this.#emitComponentUpdate(
+    this.componentEmitter.emit(
       constants.events.app.STATUS_UPDATED,
       Array.from(this.#allUsers.values())
     );
--- server/src/controller.js
+++ server/src/controller.js
@@ -55,6 +55,18 @@ export default class Controller {
     }
   }
 
+  message(socketId, data) {
+    const { userName, roomId } = this.#users.get(socketId);
+
+    this.broadcast({
+      roomId,
+      socketId,
+      event: constants.event.MESSAGE,
+      message: { userName, message: data },
+      includCurrentSocket: true,
+    });
+  }
+
   #joinUserOnRoom(roomId, user) {
     const usersOnRoom = this.#rooms.get(roomId) ?? new Map();
     usersOnRoom.set(user.id, user);
@@ -63,9 +75,26 @@ export default class Controller {
     return usersOnRoom;
   }
 
+  #loggoutUser(id, roomId) {
+    this.#users.delete(id);
+    const usersOnRoom = this.#rooms.get(roomId);
+    usersOnRoom.delete(id);
+
+    this.#rooms.set(roomId, usersOnRoom);
+  }
+
   #onSocketClosed(id) {
-    return (data) => {
-      console.log('onSocketClosed', id);
+    return (_) => {
+      const { userName, roomId } = this.#users.get(id);
+      console.log(userName, 'disconnected', id);
+
+      this.#loggoutUser(id, roomId);
+      this.broadcast({
+        roomId,
+        message: { id, userName },
+        socketId: id,
+        event: constants.event.DISCONNECT_USER,
+      });
     };
   }
 
